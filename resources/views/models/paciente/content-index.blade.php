f0a36aea217a85733f7ed4489a246312ba27948a
bolijesus <44576401+bolijesus@users.noreply.github.com>
2022-01-27 15:17:59 -0500

    Update content-index.blade.php

--- resources/views/models/paciente/content-index.blade.php
+++ resources/views/models/paciente/content-index.blade.php
@@ -25,12 +25,23 @@
                     <td>{{ $paciente->primer_nombre }}</td>
                     <td>{{ $paciente->primer_apellido }}</td>
                     <td>
-                        <a href="{{ route('backoffice.paciente.show',$paciente) }}" class="btn btn-xs bg-teal waves-effect" data-toggle="tooltip" data-placement="bottom" title data-original-title="Asistencia de citas"><i class="material-icons small">date_range</i></a>
+                        <form action="{{ route('backoffice.paciente.destroy',$paciente) }}" method="post"
+                            data-toggle="tooltip" data-placement="bottom" title data-original-title="Eliminar">
+                            @method('DELETE') @csrf
+                            <a href="{{ route('backoffice.paciente.show',$paciente) }}"
+                                class="btn btn-xs bg-teal waves-effect" data-toggle="tooltip" data-placement="bottom" title
+                                data-original-title="Asistencia de citas"><i class="material-icons small">date_range</i></a>
 
 
-                        <a href="{{ route('backoffice.paciente.edit',$paciente) }}" class="btn btn-xs btn-primary waves-effect" data-toggle="tooltip" data-placement="bottom" title data-original-title="Editar">
-                            <i class="material-icons small">mode_edit</i>
-                        </a>
+                            <a href="{{ route('backoffice.paciente.edit',$paciente) }}"
+                                class="btn btn-xs btn-primary waves-effect" data-toggle="tooltip" data-placement="bottom"
+                                title data-original-title="Editar">
+                                <i class="material-icons small">mode_edit</i>
+                            </a>
+
+                            <button type="submit"
+                                class="btn btn-xs btn-danger waves-effect "><i class="material-icons small">delete</i></button>
+                        </form>
                     </td>
                 </tr>
             @endforeach
